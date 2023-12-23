71c33f7b6da1c021f519ad49bd57c0954b783f74
AnyaSv <147295448+AnyaSv@users.noreply.github.com>
2023-12-23 16:35:52 +0330

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Arkanoid</h1>
+    <GameBoard  />
+ </template>
+ 
+ <script setup>
+ import GameBoard from './components/GameBoard.vue'
+ import { ref } from 'vue'
+ 
+ const score = ref(0)
+ 
+ function onScore()
+ {
+    score.value++
+ }
+ </script>
